ab33ea920b87d906eaa83e0bf94c84900716119a
untoha <anton-nkz@ya.ru>
2017-10-01 06:15:01 +0700

    Commit

--- app/__init__.py
+++ app/__init__.py
@@ -28,6 +28,7 @@ from ParallelSBTree import ParallelSBTree
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from app import config
 
 app = Flask(__name__)
 app.config.from_object('config')
